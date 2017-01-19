cb6119a7442e52118107036b729349065e6523dc
Gershon Hochman <gershon@lightricks.com>
2017-01-19 11:59:26 +0200

    Added configuration files for IlmBase and OpenEXR.

---
+++ OpenEXR/OpenEXRConfig.h
@@ -0,0 +1,5 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+/// This file has been intentionally remained empty. It sets all OpenEXR configurable parameters to
+/// their default values.
