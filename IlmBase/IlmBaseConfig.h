0af7de13ac12600f96a5a7bdd719d528fc1ba28b
Gershon Hochman <gershon@lightricks.com>
2017-01-25 16:07:07 +0200

    IlmBaseConfig.h: fix comments.

--- IlmBase/IlmBaseConfig.h
+++ IlmBase/IlmBaseConfig.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2017 Lightricks. All rights reserved.
 // Created by Gershon Hochman.
 
-/// This flags enables use of PThread APIs throughout the code.
+/// Enables use of pthread API throughout the code. Without this flag all operations provided by
+/// this library are single-threaded.
 #define HAVE_PTHREAD 1
